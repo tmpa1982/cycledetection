640ebd22c5a6aa05ee4d736a5def4e501d775c5b
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-06-14 19:38:12 +0800

    Bootstrap from Spring initializr

---
+++ src/test/kotlin/com/tmpa/challenge/cycledetection/CycledetectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tmpa.challenge.cycledetection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CycledetectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
