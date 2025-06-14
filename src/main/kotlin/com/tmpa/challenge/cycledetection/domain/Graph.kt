de4ae5a3179f1ae8db969090830b5a12a3643381
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-06-14 20:12:58 +0800

    Vibe cycle detection

--- src/main/kotlin/com/tmpa/challenge/cycledetection/domain/Graph.kt
+++ src/main/kotlin/com/tmpa/challenge/cycledetection/domain/Graph.kt
@@ -18,4 +18,38 @@ class Graph {
     fun getNodes(): List<Node> {
         return nodes.values.toList()
     }
+
+    fun findCycleEdges(): List<Edge> {
+        val visited = mutableSetOf<Node>()
+        val stack = mutableSetOf<Node>()
+        val cycleEdges = mutableListOf<Edge>()
+
+        fun dfs(node: Node, parent: Node?): Boolean {
+            visited.add(node)
+            stack.add(node)
+            for (neighbor in node.getEdges()) {
+                if (neighbor == parent) continue
+                if (!visited.contains(neighbor)) {
+                    if (dfs(neighbor, node)) {
+                        if (stack.contains(neighbor)) {
+                            cycleEdges.add(Edge(node, neighbor))
+                        }
+                        return true
+                    }
+                } else if (stack.contains(neighbor)) {
+                    cycleEdges.add(Edge(node, neighbor))
+                    return true
+                }
+            }
+            stack.remove(node)
+            return false
+        }
+
+        for (node in nodes.values) {
+            if (!visited.contains(node)) {
+                dfs(node, null)
+            }
+        }
+        return cycleEdges
+    }
 }
