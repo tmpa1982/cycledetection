81c1530029cfb35ef221a5bacd921a999f5beabd
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-06-29 13:26:09 +0800

    Gradle build with SpringBoot JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,20 @@ plugins {
 	kotlin("plugin.spring") version "2.1.21"
 	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
+	application
 }
 
 group = "com.tmpa.challenge"
 version = "0.0.1-SNAPSHOT"
+
+application {
+	// Set your main class here (fully qualified)
+	mainClass.set("com.tmpa.challenge.cycledetection.CycledetectionApplicationKt")
+}
+
+tasks.getByName<Jar>("jar") {
+	enabled = false
+}
 
 java {
 	toolchain {
